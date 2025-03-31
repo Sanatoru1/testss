559f1e743dd41de96c044c752ba13b905a4fdbd4
Sanatoru1 <sanatkalanchukov@gmail.com>
2025-03-31 12:00:50 +0500

    добавил пример комбинирования классов в модулях и написал комментарии к близьлежащему коду

--- sdkj/src/Button/Button.jsx
+++ sdkj/src/Button/Button.jsx
@@ -1,9 +1,12 @@
-import styles from  './Button.module.css'
-
-const Button = () => {
+import styles from './Button.module.css'
+//* в данном случае модул используется как уникальный класс который будет применяться к элементам и т.к обе эти кнопки 
+//* хоть и разные но они оба иходят от одного и того же копанента из-за этого у них всегда будет одинаковый модуль класса
+const Button = (isPrimary) => {
     return(
         <>
-        <Button className={styles.button}>Нажми меня</Button>
+        //* пример комбинирования классов в модулях
+        <button className={`${styles.button} ${isPrimary ? styles.primary : ''}`}>Нажми меня</button>
+        <button className={styles.button}>Нажми меня 2</button>
         </>
     )
 }
